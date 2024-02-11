f3aa37c580d12b3a80ccff78bfae97bdce9b8b4f
aozorarb <melon0802yota800@icloud.com>
2024-02-11 23:23:33 +0900

    separate stage

---
+++ lib/miniPLC/stage/data_file.rb
@@ -0,0 +1,44 @@
+# save and load yaml data file
+class Stage::DataFile
+  def initialize
+    require 'yaml/store'
+  end
+
+  def file_name_usable?(file_name, exist_file_ok: false)
+    if File.directory?(file_name)
+      false
+    elsif !exist_file_ok && File.exist?(file_name)
+      false
+    else
+      true
+    end
+  end
+
+
+  def save(file_name, stage_data, overwrite: false)
+    is_file_name_usable = (overwrite ? file_name_usable?(file_name, exist_file_ok: true)
+                                     : file_name_usable?(file_name))
+    raise 'file name cannot be use' unless is_file_name_usable
+    @store = YAML::Store.new(file_name)
+    @store.transaction do
+      @store['stage'] = stage_data
+    end 
+  end
+
+  def load(file_name)
+    raise 'file name cannot be use' unless file_name_usable?(file_name, exist_file_ok: true)
+    raise 'file is not exist' unless File.exist?(file_name)
+    @store = YAML::Store.new(file_name)
+    res = ''
+    @store.transaction do
+      res = @store['stage']
+    end
+    if res == ''
+      return nil
+    else
+      res
+    end
+  end
+
+  private :file_name_usable?
+end
---
+++ lib/miniPLC/stage/manager.rb
@@ -0,0 +1,46 @@
+# check that a name which given with item is not registerd
+class Stage::Manager
+  def initialize
+    # @register[item.name] = item
+    @register = {}
+  end
+
+  # check item's name has not used, and register the name. same ruby's  Set class
+  def add?(item)
+    if @register.key?(item.name)
+      nil
+    else
+      @register[item.name] = item
+    end
+  end
+
+  alias :add :add?
+
+  def delete(item)
+    if item === String
+      @register.delete(item)
+    else
+      @register.delete(item.name)
+    end
+  end
+
+  def item_exec(name, command, *args)
+    return nil unless @register.key?(name)
+    if args.size == 0
+      @register[name].method(command).call
+    else
+      @register[name].method(command).call(args)
+    end
+  end
+
+  def consist_with_stage(stage_data)
+    @register.clear
+    stage_data.each do |flow|
+      flow.each do |item|
+        add(item)
+      end
+    end
+  end
+end
+
+
