95d74e56daf187152680d5941eba7847bc24f132
aozorarb <melon0802yota800@icloud.com>
2024-02-13 02:25:38 +0900

    implement some of interactive_miniPLC

--- lib/interactive_play.rb
+++ lib/interactive_play.rb
@@ -1,9 +1,54 @@
 require 'reline'
 require_relative 'miniPLC'
 # interactive miniPLC like be irb(interactive ruby)
-puts <<EOS
-Interactive miniPLC
-You can play freely
-You should create an instance of miniPLC first
-EOS
 
+class IminiPLC
+  def initialize
+    @stty_save = `stty -g`.chomp
+    @line_no = 0
+  rescue
+    raise 'No support Environment'
+  end
+
+  def intoroduce
+    puts <<-EOS
+    Interactive miniPLC
+    EOS
+  end
+
+  def add_method_completion
+    mplc_public_methods = MiniPLC::Core.public_instance_methods
+    # public_instance_methods returns symbol
+    methods = mplc_public_methods.map(&:to_s)
+    Reline.completion_proc = proc { |word| methods }
+  end
+
+  def finalize
+    `stty #{@stty_save}` if @stty_save != nil
+  end
+
+  def input_loop
+    while line = Reline.readline("#{format("ImP:%03d", @line_no)}> ", true)
+      case line.chomp
+      when 'exit', 'quit', 'q'
+        exit 0
+      when ''
+        # NOP
+      else
+        eval(line) rescue puts $!.split('#')[0]
+      end
+      @line_no += 1
+    end
+  end
+
+  def main
+    intoroduce
+    add_method_completion
+    input_loop
+  rescue Interrupt
+    finalize
+  end
+
+end
+
+IminiPLC.new.main
--- play.rb
+++ play.rb
@@ -7,17 +7,10 @@ DStageManager = Stage.instance_variable_get('@manager')
 NUM = 1
 
 # play freely!
-case NUM
-when 1
-  plc.load('test.yml')
-when 2
-  plc.new_flow(Item::Input.new('in01'))
-  plc.push(0, Item::Timer.new('timer', 10))
-  plc.push(0, Item::Output.new('out01'))
-  plc.input_turn_on('in01')
-
-  plc.puts_state_deep
-end
-plc.save!('test.yml')
+plc.new_flow(Item::Input.new('in01'))
+plc.push(0, Item::Timer.new('timer', 10))
+plc.push(0, Item::Output.new('out01'))
+plc.input_turn_on('in01')
+plc.puts_state_deep
 
 
