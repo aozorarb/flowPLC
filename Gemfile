d68f400e122dfe7b5d1e184deb415fc2f84f9f37
aozorarb <melon0802yota800@icloud.com>
2024-05-05 20:51:31 +0900

    create test stage

--- Gemfile
+++ Gemfile
@@ -1,2 +1,3 @@
 source 'https://rubygems.org'
 gem 'reline'
+gem 'minitest'
---
+++ test/flowPLC/test_stage.rb
@@ -0,0 +1,23 @@
+require 'minitest/autorun'
+require_relative '../../lib/flowPLC/stage'
+
+class FlowPLC::Stage::Test < Minitest::Test
+  def setup
+    @stage = FlowPLC::Stage.new
+  end
+  
+  def test_push_item
+    
+  end
+
+  def test_insert_item
+
+  end
+
+  def test_new_flow
+
+  end
+
+
+end
+
